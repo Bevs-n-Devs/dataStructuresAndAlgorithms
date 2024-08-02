f385be0814b2af68fb892109ba3ffbf86ac6b47c
NerdboyQ <princtonb@gmail.com>
2024-08-02 15:19:57 -0700

    adding helper files for common functions and data structures

---
+++ c++/Utils/utils.cpp
@@ -0,0 +1,97 @@
+#include "utils.h"
+
+LListNode::LListNode(int v) {
+    this->value = v;
+    this->next = nullptr;
+}
+
+LListNode::LListNode(int v, int n) {
+    this->value = v;
+    this->next = new LListNode(n);
+}
+
+LListNode::LListNode(std::vector<int> vals) {
+    this->value = vals[0];
+    this->next = new LListNode({vals.begin()+1,vals.end()});
+}
+
+void LListNode::print() {
+    std::cout << "[" << this->value;
+    LListNode* nxt = this->next;
+    while (nxt) {
+        std::cout << "->" << nxt->value;
+    }
+    std::cout << "]";
+}
+
+DLListNode::DLListNode(int v) {
+    this->value = v;
+    this->next = nullptr;
+    this->prev = nullptr;
+}
+
+DLListNode::DLListNode(int v, int n) {
+    this->value = v;
+    this->next = new DLListNode(n);
+    this->prev = nullptr;
+}
+
+DLListNode::DLListNode(int v, std::vector<int> vals) {
+    this->value = v;
+    int n = vals[0];
+    this->next = new DLListNode(n, {vals.begin()+1,vals.end()});
+}
+
+void DLListNode::print() {
+    std::cout << "[" << this->value;
+    DLListNode* nxt = this->next;
+    while (nxt) {
+        std::cout << "->" << nxt->value;
+    }
+    std::cout << "]";
+}
+
+void printVector_Chr(std::vector<char> v) {
+    std::cout << "[" << *v.begin();
+    
+    for(int i=1; i<v.size();i++) {
+        std::cout << "," << v[i];
+    }
+    std::cout << "]";
+}
+
+void printVector_Int(std::vector<int> v) {
+    std::cout << "[" << *v.begin();
+    
+    for(int i=1; i<v.size();i++) {
+        std::cout << "," << v[i];
+    }
+    std::cout << "]";
+}
+
+void printVector_Str(std::vector<char*> v) {
+    std::cout << "[" << *v.begin();
+    
+    for(int i=1; i<v.size();i++) {
+        std::cout << "," << v[i];
+    }
+    std::cout << "]";
+}
+
+void printArray_Int(int* arr, int N) {
+    std::cout << "[" << arr[0];
+    
+    for(int i=1; i<N;i++) {
+        std::cout << "," << arr[i];
+    }
+    std::cout << "]";
+}
+
+void printArray_Chr(char* arr, int N) {
+    std::cout << "[" << arr[0];
+    
+    for(int i=1; i<N;i++) {
+        std::cout << "," << arr[i];
+    }
+    std::cout << "]";
+}
---
+++ c++/Utils/utils.h
@@ -0,0 +1,164 @@
+#ifndef __UtilsHelper__
+#define __UtilsHelper__
+
+#include <iostream>
+#include <vector>
+
+/**
+ * Linked List Object
+ */
+struct LListNode {
+    int value;
+    LListNode* next;
+
+    /**
+     * Creates Linked List with initial value
+     * 
+     * @param v: integer value for the node to store
+     */
+    LListNode(int v=0);
+
+    /**
+     * Creates Linked List with initial value
+     * 
+     * @param v: integer value for the main node to store
+     * @param n: integer value for the next node to store
+     */
+    LListNode(int v, int n);
+
+    /**
+     * Creates Linked List with initial value
+     * 
+     * @param vals: vector of integer values for the remaining nodes to store
+     */
+    LListNode(std::vector<int> vals);
+
+    /**
+     * Destructor to free memory
+     */
+    ~LListNode();
+
+    /**
+     * Prints the LList values
+     */
+    void print();
+};
+
+/**
+ * Doubly Linked List Object
+ */
+struct DLListNode {
+    int value;
+    DLListNode* prev;
+    DLListNode* next;
+
+    /**
+     * Creates Doubly Linked List with initial value
+     * 
+     * @param v: integer value for the node to store
+     */
+    DLListNode(int v);
+
+    /**
+     * Creates Doubly Linked List with initial value
+     * 
+     * @param v: integer value for the main node to store
+     * @param n: integer value for the next node to store
+     */
+    DLListNode(int v, int n);
+
+    /**
+     * Creates Doubly Linked List with initial value
+     * 
+     * @param v: integer value for the main node to store
+     * @param p: integer value for the prev node to store
+     * @param n: integer value for the next node to store
+     */
+    DLListNode(int v, int p, int n);
+
+    /**
+     * Creates Doubly Linked List with initial value
+     * 
+     * @param v: integer value for the main node to store
+     * @param vals: vector of integer values for the remaining nodes to store
+     */
+    DLListNode(int v, std::vector<int> vals);
+
+    /**
+     * Destructor to free memory
+     */
+    ~DLListNode();
+
+    /**
+     * Prints the DLList values
+     */
+    void print();
+};
+
+struct BinaryTreeNode {
+    int data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+
+    /**
+     * Creates Binary Tree Node with initial value
+     * 
+     * @param v: integer value for the node to store
+     */
+    BinaryTreeNode(int v);
+
+    /**
+     * Creates Binary Tree Node with initial value
+     * 
+     * @param v: integer value for the main node to store
+     * @param n: integer value for the next node to store
+     */
+    BinaryTreeNode(int v, int n);
+
+    /**
+     * Creates Doubly Linked List with initial value
+     * 
+     * @param v: integer value for the main node to store
+     * @param vals: vector of integer values for the remaining nodes to store
+     */
+    BinaryTreeNode(int v, std::vector<int> vals);
+};
+
+/**
+ * Prints vector of Integers
+ * 
+ * @param v: Vector of integers
+ */
+void printVector_Int(std::vector<int> v);
+
+/**
+ * Prints vector of Characters
+ * 
+ * @param v: Vector of characters
+ */
+void printVector_Chr(std::vector<char> v);
+
+/**
+ * Prints vector of Strings
+ * 
+ * @param v: Vector of strings
+ */
+void printVector_Str(std::vector<char*> v);
+
+/**
+ * Prints Array of Integers
+ * 
+ * @param v: Array of integers
+ * @param N: Number of elements in the array
+ */
+void printArray_Int(int *arr, int N);
+
+/**
+ * Prints Array of Characters
+ * 
+ * @param v: Array of characters
+ * @param N: Number of elements in the array
+ */
+void printArray_Chr(char *arr, int N);
+
+#endif
