759e506e04d098b21bd39b8679cc891bcc9a8545
NerdboyQ <princtonb@gmail.com>
2024-08-09 15:29:22 -0700

    added problem description

--- c++/LeetCode/ToeplitzMatrix.cpp
+++ c++/LeetCode/ToeplitzMatrix.cpp
@@ -2,11 +2,44 @@
 #include <set>
 #include "../Utils/utils.h"
 
+
+/**
+ * Given an m x n matrix, return true if the matrix is Toeplitz. 
+ * Otherwise, return false. A matrix is Toeplitz if every diagonal 
+ * from top-left to bottom-right has the same elements.
+ * 
+ * E.g.
+ * Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+ * [
+ *  [1 2 3 4],
+ *  [5 1 2 3],
+ *  [9 5 1 2]
+ * ]
+ * Output: true
+ * Explanation:
+ * In the above grid, the diagonals are:
+ * "[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+ * In each diagonal all elements are the same, so the answer is True.
+ * 
+ * constraints:
+ *      * m == matrix.length
+ *      * n == matrix[i].length
+ *      * 1 <= m, n <= 20
+ *      * 0 <= matrix[i][j] <= 99
+ * 
+ */
+
+/**
+ * Test Object
+ */
 typedef struct{
     std::vector<std::vector<int>> matrix;
     bool expectedResult;
 }Test;
 
+/**
+ * TestCases for solution
+ */
 std::vector<Test> TestCases = {
     Test{{{1,2,3,4},{5,1,2,3},{9,5,1,2}}, true},
     Test{{{1,2},{2,2}}, false},
