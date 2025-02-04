dae8ff2ceb51370897fb4cb386c19f92e969725a
NerdboyQ <princtonb@gmail.com>
2025-02-03 18:41:34 -0800

    updated details on 33 Leetcode Problem solution & README help

---
+++ python/Leetcode-Problems/README.md
@@ -0,0 +1,17 @@
+# Leetcode Problems
+
+The following are solutions created in Python for different LeetCode problems. These aren't always considered the "most 
+right" or "most efficient" solutions, they are simply our
+member's contributions so feel free to add as you'd like.
+
+> NOTE: Some of the solutions are written with pytest. If you 
+would like to run them using pytest, please install pytest with 
+`python -m pip install pytest` first, and then tests can be run
+with the following commands:
+   * runs all the solutions in this directory   
+   `pytest ../Leetcode-Problems`
+      > NOTE: pytest recognizes files prefixed with <i>"test_"</i>
+   * runs a specific problem's solution   
+   `pytest test_12_Integer_to_Roman.py`
+   * shows more details in the terminal when running all the tests, to help with debug if necessary.   
+   `pytest ../Leetcode-Problems --verbose`
--- python/Leetcode-Problems/test_33_Search_in_Rotated_Sorted_Array.py
+++ python/Leetcode-Problems/test_33_Search_in_Rotated_Sorted_Array.py
@@ -35,12 +35,27 @@ Constraints:
     * nums is an ascending array that is possibly rotated.
     * -10^4 <= target <= 10^4
 """
+
+
 class TESTCASE:
-    def __init__(self, nums:int, target:int, expectedResult:int) -> int:
+    """
+    Object to hold TestCases
+    """
+    def __init__(self, nums:int, target:int, expectedResult:int) -> None:
+        """
+        Initializes the TestCase object
+
+        @param nums: list of numbers to search
+        @param target: value to search for in the list
+        @param expectedResult: the correct answer for the TestCase
+        """
         self.nums = nums
         self.target = target
         self.expectedResult = expectedResult
 
+"""
+List of Test Cases to test the problem solution
+"""
 TestCases = [
     TESTCASE([4,5,6,7,0,1,2], 0, 4),
     TESTCASE([4,5,6,7,0,1,2], 3, -1),
@@ -50,43 +65,79 @@ TestCases = [
     TESTCASE([1,3,5], 3, 1),
     TESTCASE([3,5,1], 1, 2),
 ]
+
 class Solution:
+    """
+    Solution for LeetCode Problem
+    """
     def search(nums: list[int], target: int) -> int:
+        """
+        This solution uses a binary search approach to search
+        for the given target value.
+
+        @param nums: list of values to search
+        @param target: value to search for
+
+        @return index of the given target value if found, -1 will 
+        be returne if the target isn't found or invalid params were 
+        provided
+        """
+
+        # Check Constraints
         if len(nums) < 1 or len(nums) > 5000:
             return -1
         if target < -(10**4) or target > 10**4:
             return -1
 
-
+        # Set pointers for binary search
         l = 0
         r = len(nums)-1
 
+        # loop until a value is found or until the
+        # range has been exceeded
         while l <= r :
+            # constraints check
             if target > nums[r] and target < nums[l]:
                 return -1
+            
+            # returns values if found
             if (nums[l] == target):
                 return l
             if (nums[r] == target):
                 return r
 
+            # catchs condition when there are not values found
             if r-l < 2:
                 return -1
             
+            # finds the center index
             c = (r+l)//2
+            
+            # checks the center value for a match
             if nums[c] == target:
                 return c
 
+            # checks positions when there is a rotation that
+            # has occurred in the list range
             if nums[l] <= nums[c]:
+                # If the target is greater than the center
+                # value, search the right side of the list
                 if target < nums[l] or target > nums[c]:
                     l = c+1
                     r-=1
+                # If the target is less than the center
+                # value search the left side of the list
                 else:
                     r = c-1
                     l+=1 
             else:
+                # If the target is greater than the center
+                # value, search the right side of the list
                 if target < nums[c] or target > nums[r]:
                     r = c-1
                     l+=1 
+                # If the target is less than the center
+                # value search the left side of the list
                 else:  
                     l = c+1
                     r-=1 
